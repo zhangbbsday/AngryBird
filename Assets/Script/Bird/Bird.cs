cd060a9382784b968d887ab6c577afb2f1623ac3
zhangbbsday <1291238048@qq.com>
2019-11-10 16:46:13 +0800

    Add two scripts.

---
+++ Assets/Script/Bird/Bird.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public enum State
+    {
+        OnGround = 0,
+        OnSlingShot = 1,
+        Shot = 2,
+        InSky = 3,
+        Collision = 4,
+        Scroll = 5,
+        Dead = 6
+    }
+
+    public AudioClip NormalSong;
+    public AudioClip ChoseSong;
+    public AudioClip ShotSong;
+    public AudioClip CollisionSong;
+    public AudioClip SkillSong;
+    public AudioClip DestorySong;
+
+    protected int hp;
+    protected int[] damage;
+    protected int weight;
+    protected AudioSource audioSource;
+    protected Animator animator;
+    protected float existTime;
+
+    public int Hp { get; set; }
+    public int[] Damage { get; protected set; }
+    public int Weight { get; protected set; }
+    public Rigidbody2D RigidbodySelf { get; protected set; }
+    public State BirdState { get; set; }
+
+    
+    protected void Start()
+    {
+        Initialize();
+    }
+
+    protected void Update()
+    {
+        UpdataThings();
+    }
+    public void DestroyThis()
+    {
+        Destroy(gameObject);
+    }
+
+    protected virtual void Initialize()
+    {
+        RigidbodySelf = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
+        BirdState = State.OnGround;
+    }
+    protected virtual void UseSkill()
+    {
+        if (Input.GetMouseButtonDown(0))
+            audioSource.PlayOneShot(SkillSong);
+
+        //使用技能
+    }
+    protected virtual void DestroySelf()
+    {
+        audioSource.PlayOneShot(DestorySong);
+
+        //播放销毁动画，交由动画动作销毁物体
+    }
+
+    protected virtual void UpdataThings()
+    {
+
+    }
+
+    protected IEnumerable Dead()
+    {
+        yield return new WaitForSeconds(existTime);
+        BirdState = State.Dead;
+        DestroySelf();
+    }
+}
---
+++ Assets/Script/Pig/Pig.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pig : MonoBehaviour
+{
+    public int Hp { get; set; }
+    public Rigidbody2D RigidbodySelf { get; private set; }
+
+    private Animator animator;
+    private AudioSource audioSource;
+    void Start()
+    {
+        Initialize();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    protected virtual void Initialize()
+    {
+        RigidbodySelf = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
+    }
+}
